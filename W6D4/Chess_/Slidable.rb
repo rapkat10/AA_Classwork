3337914acc76def996ebf1e365e579cc02a6be02
rapkat10 <aminrapkat@gmail.com>
2019-11-21 18:41:19 -0700

    Chess phase 2

---
+++ W6D4/Chess_/Slidable.rb
@@ -0,0 +1,29 @@
+
+module Slidable
+
+    def horizontal_dirs 
+        HORIZONTAL_DIRS
+    end
+
+    def diagonal_dirs 
+        DIAGONAL_DIRS
+    end
+
+    def moves
+        
+    end
+
+    private
+
+    HORIZONTAL_DIRS = [[1, 0], [0, 1], [-1, 0], [0, -1]] # FIGURE OUT LATER
+    DIAGONAL_DIRS = [[1, 1], [-1, 1], [1, -1], [-1, -1]] # figure out later
+
+    def move_dirs
+
+    end
+
+    def grow_unblocked_moves_in_dir(dx, dy)
+
+    end
+
+end
