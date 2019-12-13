d0bcc57db5b0f03e8659c7387c71ece7a3a1dbec
rapkat10 <aminrapkat@gmail.com>
2019-12-12 19:39:47 -0700

    99_cats_Auth fixed users route

--- Week 9/W9D4/99_cats_Auth/app/controllers/users_controller.rb
+++ Week 9/W9D4/99_cats_Auth/app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+    def index
+        @users = User.all
+        render json: @users
+    end
+
     def new
         @user = User.new
         render :new # make sure to add new.html.erb in the views
