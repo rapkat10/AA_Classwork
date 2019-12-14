4c887489370403f7b68b87e8204587229ee983f0
rapkat10 <aminrapkat@gmail.com>
2019-12-13 21:34:21 -0700

    Music_Rapp project

--- Week 9/W9D5/MusicRapp/app/models/user.rb
+++ Week 9/W9D5/MusicRapp/app/models/user.rb
@@ -1,3 +1,17 @@
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer(8)      not null, primary key
+#  email           :string          not null
+#  password_digest :string          not null
+#  session_token   :string          not null
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#
+
+
 class User < ApplicationRecord
 
     validates :email, presence: true, uniqueness: true
@@ -13,45 +27,32 @@ class User < ApplicationRecord
         user = User.find_by(email: email)
         return nil if user.nil?
         user.is_password?(password) ? user : nil
-    end
+    end    
     
     def is_password?(password)
         bcrypt_password = BCrypt::Password.new(self.password_digest)
         bcrypt_password.is_password?(password)
-    end
+    end    
 
     def password=(password)
         @password = password
         self.password_digest = BCrypt::Password.create(password)
-    end
+    end    
 
     def reset_session_token!
         self.session_token = self.class.generate_session_token
         self.save!
         self.session_token
-    end
+    end    
 
     def self.generate_session_token
         SecureRandom::urlsafe_base64(16)
-    end
+    end    
 
     private
 
     def ensure_session_token
         self.session_token ||= self.class.generate_session_token
-    end
+    end    
 
-end
-
-# == Schema Information
-#
-# Table name: users
-#
-#  id              :integer(8)      not null, primary key
-#  email           :string          not null
-#  password_digest :string          not null
-#  session_token   :string          not null
-#  created_at      :datetime        not null
-#  updated_at      :datetime        not null
-#
-
+end    
