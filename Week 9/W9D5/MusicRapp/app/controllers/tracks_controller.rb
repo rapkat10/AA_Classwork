68e62470675257d93fabbc3ba639c273798e0fa9
rapkat10 <aminrapkat@gmail.com>
2019-12-14 14:25:21 -0700

    Music_Rapp project edited tracks feature

--- Week 9/W9D5/MusicRapp/app/controllers/tracks_controller.rb
+++ Week 9/W9D5/MusicRapp/app/controllers/tracks_controller.rb
@@ -31,7 +31,7 @@ class TracksController < ApplicationController
 
     def update
         @track = Track.find(params[:id])
-        if @track.update_attributes(track_params)
+        if @track.update(track_params)
             redirect_to track_url(@track)
         else
             flash.now[:errors] = @track.errors.full_messages
