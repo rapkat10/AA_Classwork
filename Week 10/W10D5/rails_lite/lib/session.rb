9406a1d6223350de7ba97f1e00f8834c7bd88f3e
rapkat10 <aminrapkat@gmail.com>
2019-12-20 17:39:37 -0700

    rails_lite

---
+++ Week 10/W10D5/rails_lite/lib/session.rb
@@ -0,0 +1,24 @@
+require 'json'
+
+class Session
+  # find the cookie for this app
+  # deserialize the cookie into a hash
+  
+  def initialize(req)
+    @session = req.cookies["_rails_lite_app"]
+    @session = @session.nil? ? {} : JSON.parse(@session)
+  end
+
+  def [](key)
+    @session[key]
+  end
+
+  def []=(key, val)
+    @session[key] = val
+  end
+
+  def store_session(res)
+    cookie = @session.to_json
+    res.set_cookie("_rails_lite_app", cookie)
+  end
+end
