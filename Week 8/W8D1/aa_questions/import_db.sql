18bf7bb1578913df0a6e1f07511ecf6e65594687
rapkat10 <aminrapkat@gmail.com>
2020-01-13 14:17:36 -0700

    created questions table

--- Week 8/W8D1/aa_questions/import_db.sql
+++ Week 8/W8D1/aa_questions/import_db.sql
@@ -8,3 +8,12 @@ CREATE TABLE users (
     lname TEXT NOT NULL
 );
 
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+
