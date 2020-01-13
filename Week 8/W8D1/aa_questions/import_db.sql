d47ed0337488837a19d91135f42b25ac397faaf0
rapkat10 <aminrapkat@gmail.com>
2020-01-13 14:54:29 -0700

    done with creating tables

--- Week 8/W8D1/aa_questions/import_db.sql
+++ Week 8/W8D1/aa_questions/import_db.sql
@@ -12,8 +12,8 @@ CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    author_id INTEGER NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES users(id)
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE question_follows (
@@ -36,3 +36,57 @@ CREATE TABLE replies (
     FOREIGN KEY (reply_id) REFERENCES replies(id)
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+    ('Rapkat', 'Amin'), ('App', 'Academy');
+
+INSERT INTO
+  questions (title, body, user_id)
+VALUES
+    ('Sports', 'Whats your favorite sport?', 1),
+    ('Country', 'Which country do you wish to live in?', 2)
+
+
+INSERT INTO
+    replies (body, question_id, reply_id, user_id)
+VALUES
+    (
+        ('Soccer is my favorite sport!'),
+        (SELECT id
+        from questions
+        WHERE title = 'Sports'),
+        (SELECT reply_id
+        FROM replies),
+        (SELECT id
+        FROM users
+        WHERE fname = 'Rapkat')
+    ),
+
+    (
+        ('Not a specific country, but maybe a country in Europe!'),
+        (SELECT id
+        from questions
+        WHERE title = 'Country'),
+        (SELECT reply_id
+        FROM replies),
+        (SELECT id
+        FROM users
+        WHERE fname = 'App')
+    );
+
+INSERT INTO
+    question_likes (user_id, question_id)
+VALUES
+    (1, 1),
+    (2, 2);
