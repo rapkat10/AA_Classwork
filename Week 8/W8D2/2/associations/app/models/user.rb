a613dcaeff0598792d627740cd55d2b8ea6a1c4f
rapkat10 <aminrapkat@gmail.com>
2020-01-14 17:50:08 -0700

    associations project

---
+++ Week 8/W8D2/2/associations/app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ApplicationRecord
+
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_courses,
+        through: :enrollments,
+        source: :course
+
+
+    has_many :courses,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :Course
+    
+end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer(8)      not null, primary key
+#  name       :string
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
