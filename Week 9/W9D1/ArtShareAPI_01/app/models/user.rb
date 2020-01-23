78f16e32f2dabb24a9f8e2f5e21ed6fe29dc6911
rapkat10 <aminrapkat@gmail.com>
2020-01-22 18:10:04 -0700

    created models

---
+++ Week 9/W9D1/ArtShareAPI_01/app/models/user.rb
@@ -0,0 +1,30 @@
+class User < ApplicationRecord
+
+    validates :username, presence: true, uniqueness: true 
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer(8)      not null, primary key
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#  username   :string          not null
+
+    has_many :artworks, 
+        dependent: :destroy,
+        foreign_key: :artist_id, 
+        primary_key: :id, 
+        class_name: :Artwork  
+
+    has_many :shares,
+        dependent: :destroy,
+        foreign_key: :viewer_id,
+        primary_key: :id,
+        class_name: :ArtworkShare
+
+    has_many :shared_artworks,
+        through: :shares,
+        source: :artwork
+
+end 
