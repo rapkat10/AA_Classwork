bafbb2422b44059381bd8fbf997f5c0df4b56d85
rapkat10 <aminrapkat@gmail.com>
2020-01-24 17:57:28 -0700

    created routes

---
+++ Week 9/W9D4/99_cats_auth_2/config/routes.rb
@@ -0,0 +1,16 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+Rails.application.routes.draw do
+  resources :cats, except: :destroy
+  resources :cat_rental_requests, only: [:new, :create] do
+    member do
+      post :approve
+      post :deny
+    end
+  end
+  
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+  
+  root to: redirect('/cats')
+end
