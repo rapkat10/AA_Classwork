9a8ff1c32ae86c6e8bfed13cc698bf4503e140f3
rapkat10 <aminrapkat@gmail.com>
2020-01-30 17:55:11 -0700

    started phase 2 subs and posts

--- Week 10/W10D4/RedditClone/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  # root to: redirect('/subs')
-  
-  resources :users, only: [:new, :create]
-  resource :session, only: [:new, :create, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
