e020efebe9a13ecb4ae6a7eb9aa59b5a4428dc9b
rapkat10 <aminrapkat@gmail.com>
2020-01-30 17:50:22 -0700

    created models

---
+++ Week 10/W10D4/RedditClone/app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    attr_reader :password
+
+    after_initialize :ensure_session_token
+    #FGRIPE
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return user if user && self.is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom.urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    private
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+end
