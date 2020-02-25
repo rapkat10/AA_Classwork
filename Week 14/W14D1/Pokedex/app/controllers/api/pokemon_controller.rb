91bfd97f41100cbf38f3dfe966a04a93629eecd9
rapkat10 <aminrapkat@gmail.com>
2020-02-24 18:17:45 -0700

    finished pokedex part 1

---
+++ Week 14/W14D1/Pokedex/app/controllers/api/pokemon_controller.rb
@@ -0,0 +1,22 @@
+class Api::PokemonController < ApplicationController
+
+  def index
+    @pokemon = Pokemon.all
+    render :index
+  end
+
+  def show
+    @pokemon = Pokemon.find(params[:id])
+    render :show
+  end
+
+  def create
+
+  end
+
+  private
+  def pokemon_params
+    params.require("pokemon").permit(:name, :attack, :defense, :poke_type)
+  end
+
+end
