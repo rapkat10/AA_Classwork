d114bf5aef8d4fe67106cd13f4965d2829e52eb4
rapkat10 <aminrapkat@gmail.com>
2020-03-29 23:56:59 -0600

    graphql day 2 project

---
+++ GraphQL/Day 2/online_store/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI
+require('./models');
+const graphQLHTTP = require('express-graphql');
+const expressPlayground = require('graphql-playground-middleware-express').default;
+const { schema, resolvers } = require('./schema');
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(() => console.log('Connected to MongoDB successfully'))
+  .catch(err => console.log(err));
+  
+  app.use(
+  '/graphql',
+  graphQLHTTP({
+    schema: schema,
+    rootValue: resolvers
+  })
+);
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
+app.get('/hello', (req, res) => res.send('Hello World!'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
